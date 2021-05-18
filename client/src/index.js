d9ab55b3f3c825ad43486383afc3ba5d8bef32f7
akshadagrawal <akshadagrawal.aa@gmail.com>
2021-05-18 21:14:18 +0530

    .

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from 'react-redux';
 import store from './redux/configureStore'; 
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
 
 
 import App from './App';
 
 ReactDOM.render(
-  <Provider store= {store}>
-     <App />
-  </Provider>,
+  <Router>
+    <Provider store= {store}>
+      <App />
+    </Provider>
+  </Router>,
    
   document.getElementById('root')
 );
